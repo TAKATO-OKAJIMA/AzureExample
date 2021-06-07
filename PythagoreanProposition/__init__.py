23432c5a8eba1447d2a9f9e73b827d90de409f1a
岡嶋 隆人 <1175052@cstu.nit.ac.jp>
2021-06-07 22:49:44 +0900

    fixed bugs

--- PythagoreanProposition/__init__.py
+++ PythagoreanProposition/__init__.py
@@ -11,8 +11,22 @@ SIDE_C = 'c'
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    side_a = int(req.params.get(SIDE_A))
-    side_b = int(req.params.get(SIDE_B))
+    query_a = req.params.get(SIDE_A)
+    query_b = req.params.get(SIDE_B)
+
+    try:
+        side_a = int(query_a)
+        side_b = int(query_b)
+    except TypeError:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+    except ValueError:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
 
     if not (side_a and side_b):
         try:
